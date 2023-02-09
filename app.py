01bd3b8fc3e7a54dea4267520cc07ff6d18e9c90
Semih Orhan <sorhan3.14@gmail.com>
2023-02-09 13:42:31 +0300

    activate debug mode

--- app.py
+++ app.py
@@ -2,30 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!?!?!?!'
 
 @app.route('/test/<search_query>')
 def search(search_query):
     return search_query
-
-@app.route('/integer/<int:value>')
-def int_type(value):
-    print(value + 1)
-    return 'correct'
-
-@app.route('/float/<float:value>')
-def float_type(value):
-    print(value + 1)
-    return 'correct'
-
-# dynamic route that accepts slashes
-@app.route('/path/<path:value>')
-def path_type(value):
-    print(value)
-    return 'correct'
 
 @app.route('/name/<name>')
 def index(name):
